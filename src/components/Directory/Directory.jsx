3b1ba121018669a7182ef08e3006b3886212d36b
John Trujillo <amriikk@gmail.com>
2021-04-24 00:15:26 -0700

    update state for sections

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,7 +1,54 @@
 import React, { Component } from 'react';
+import MenuItem from '../Menu-Item/Menu-Item';
+import './Directory.scss'
 
 class Directory extends Component {
-    
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [
+                {
+                  title: 'hats',
+                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                  id: 1
+                },
+                {
+                  title: 'jackets',
+                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                  id: 2
+                },
+                {
+                  title: 'sneakers',
+                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                  id: 3
+                },
+                {
+                  title: 'womens',
+                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                  size: 'large',
+                  id: 4
+                },
+                {
+                  title: 'mens',
+                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                  size: 'large',
+                  id: 5
+                }
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.sections.map( ({ title, imageUrl, id }) => (
+                    <MenuItem key={id} title={title} />
+                ))}
+            </div>
+        )
+    }
 }
+
 
 export default Directory;
