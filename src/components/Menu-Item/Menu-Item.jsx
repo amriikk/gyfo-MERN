127bd9a06f535cad83793a550c195bdc7e5ced6d
John Trujillo <amriikk@gmail.com>
2021-04-25 23:25:40 -0700

    inline styling

--- src/components/Menu-Item/Menu-Item.jsx
+++ src/components/Menu-Item/Menu-Item.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl }) => (
+    <div 
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+        className='menu-item'
+    >
         <div className='content'>
             <div className='title'>{title}</div>
             <span className='subtitle'>SHOP NOW</span>
